e4cfb7c24ad3cd2b0bd747d15f980007fcb96b63
Jose Palacios <tex@Joses-MacBook-Air.local>
2021-05-02 13:12:07 -0500

    working cmake config

---
+++ libraries/src/list.cpp
@@ -0,0 +1,5 @@
+#include "libraries/list.h"
+#include <iostream>
+
+void List::print() { std::cout << "Hello sub-library 1!" << std::endl; }
+
---
+++ standalone/main.cpp
@@ -0,0 +1,9 @@
+#include "libraries/list.h"
+
+int main(int argc, char *argv[])
+{
+    List hi;
+    hi.print();
+
+    return 0;
+}
